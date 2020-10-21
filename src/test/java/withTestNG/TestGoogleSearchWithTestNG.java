17662c0f2e49e4a224ea6883c1f7804826374c30
RameshTulal <rameshtulal@yahoo.com>
2020-10-21 17:57:05 +0200

    commit

--- src/test/java/withTestNG/TestGoogleSearchWithTestNG.java
+++ src/test/java/withTestNG/TestGoogleSearchWithTestNG.java
@@ -5,6 +5,7 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+
 public class TestGoogleSearchWithTestNG {
 	WebDriver driver = null;
 	public static String BrowserName = null;
