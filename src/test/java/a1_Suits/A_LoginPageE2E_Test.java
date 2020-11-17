23022fb135607527e61ace82d48aefcaf13886f1
RameshTulal <rameshtulal@yahoo.com>
2020-11-17 13:01:35 +0100

    test

--- src/test/java/a1_Suits/A_LoginPageE2E_Test.java
+++ src/test/java/a1_Suits/A_LoginPageE2E_Test.java
@@ -2,8 +2,11 @@ package a1_Suits;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -13,46 +16,52 @@ public class A_LoginPageE2E_Test {
 	WebDriver driver;
 	
 	@BeforeMethod
-	public void setUp() {	
+	public void setUp() throws InterruptedException {	
 		String ProjectPath = System.getProperty("user.dir");
-		System.setProperty("webdriver.chrome.driver",ProjectPath+"/ChromeDriver/85/chromedriver.exe");		
-		driver = new ChromeDriver();	
+		System.setProperty("webdriver.chrome.driver",ProjectPath+"/ChromeDriver/85/chromedriver.exe");	
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--disable-notifications");
+		driver = new ChromeDriver(options);	
 		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-		driver.get("https:/www.google.co.in");
+		driver.get("https://www.facebook.com/");
+		driver.manage().window().maximize();
+		Thread.sleep(2000);
+		WebElement pop = driver.findElement(By.xpath("//*[@id=\'u_0_h\']"));
+		pop.click();
 		System.out.println("@BeforeMethod: Application Launched..");
 	}	
 	
 	@Test(groups = { "Login" })
 	public void verfyLoginPageDisplayed() {	
-		driver.manage().window().maximize();	
-		Assert.assertEquals("test", "test","Login page not found..!");		
-		System.out.println("test verfyLoginPageDisplayed executed");	
+		WebElement txtUserName = driver.findElement(By.xpath("//input[@id='email']"));		
+		WebElement txtPassword = driver.findElement(By.xpath("//input[@id='pass']"));		
+		WebElement btnLogin = driver.findElement(By.xpath("//*[@id=\'u_0_b\']"));
+		String str = btnLogin.getAttribute("name");	
+		
+		if(txtUserName.isDisplayed()&&txtPassword.isDisplayed()&&btnLogin.isDisplayed())			
+			Assert.assertEquals(str, "login","Login page not found..!");		
+		System.out.println("test: verfyLoginPageDisplayed executed");	
 	}
 	
-	@Test(dependsOnMethods = { "verfyLoginPageDisplayed" }, groups = { "Login" })
-	public void verfyUserNameTextBox() {
-		driver.manage().window().maximize();	
-		Assert.assertEquals("test", "test","UserName TextBox is not found..!");	
-		System.out.println("test verfyUserNameTextBox executed");	
-	}
 	
 	@Test(dependsOnMethods = { "verfyLoginPageDisplayed" }, groups = { "Login" })
-	public void verfyPasswordTextBox() {	
-		driver.manage().window().maximize();	
-		Assert.assertEquals("test", "test","Password TextBox is not found..!");	
-		System.out.println("test verfyPasswordTextBox executed");	
-	}
-	
-	@Test(dependsOnMethods = { "verfyLoginPageDisplayed" }, groups = { "Login" })
-	public void verfyLoginButton() {	
-		driver.manage().window().maximize();	
-		Assert.assertEquals("test", "test","Login Button is not found..!");	
+	public void Login() {	
+		WebElement txtUserName = driver.findElement(By.xpath("//input[@id='email']"));
+		WebElement txtPassword = driver.findElement(By.xpath("//input[@id='pass']"));
+		WebElement btnLogin = driver.findElement(By.xpath("//*[@id=\'u_0_b\']"));
+		txtUserName.sendKeys("465282049");
+		txtPassword.sendKeys("HighoneJ@1234");
+		btnLogin.click();			
+		
+		WebElement friends = driver.findElement(By.xpath("//span[contains(text(),'Create a Story')]"));
+		if(friends.isDisplayed())
+			Assert.assertEquals(friends.getText(), "Create a Story","Login failed..!");	
 		System.out.println("test verfyLoginButton executed");	
 	}
 	
 	@AfterMethod
 	public void tearDown() {
-		driver.close();
+		//driver.close();
 		System.out.println("@AfterMethod: Application Closed..");		
 	}		
 			
--- src/test/java/a1_Suits/C_LogoutE2E_Test.java
+++ src/test/java/a1_Suits/C_LogoutE2E_Test.java
@@ -1,12 +1,8 @@
 package a1_Suits;
 
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 
@@ -14,31 +10,22 @@ public class C_LogoutE2E_Test {
 	WebDriver driver = null;
 	public static String BrowserName = null;
 	
-	@BeforeMethod
-	public void setUp() {	
-		String ProjectPath = System.getProperty("user.dir");
-		System.setProperty("webdriver.chrome.driver",ProjectPath+"/ChromeDriver/85/chromedriver.exe");		
-		driver = new ChromeDriver();	
-		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-		driver.get("https:/www.google.co.in");	
-		System.out.println("@BeforeMethod: Application Launched..");	
-	}	
 
-	@Test(priority=0)
+	@Test(priority=0, dependsOnMethods="Login")
 	public void verfyLogoutPageDisplayed() throws Exception {	
 		Assert.assertEquals("test", "test","Logout page not found..!");	
 		System.out.println("Test verfyLogoutPageDisplayed is executed...");
 	}
 	
-	@Test(priority=1)
-	public void verfyLogoutButton() throws Exception {
-		Assert.assertEquals("test", "test","Logout button not found..!");	
+	@Test(priority=1, dependsOnMethods="verfyLogoutPageDisplayed")
+	public void Logout() throws Exception {
+		Assert.assertEquals("test", "test","Logout failed..!");	
 		System.out.println("Test verfyLogoutButton is executed...");
 	}
 	
 	@AfterMethod
 	public void tearDown() {
-		driver.close();
+		//driver.close();
 		System.out.println("@AfterMethod: Application Closed..");		
 	}
 	
